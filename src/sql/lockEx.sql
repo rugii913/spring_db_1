5f47d9a24184e2f35fc13d03ccc5c5539e7b1e18
rugii913 <rugii913@gmail.com>
2023-08-25 17:40:26 +0900

    DB 락-조회

--- src/sql/lockEx.sql
+++ src/sql/lockEx.sql
@@ -1,3 +1,4 @@
+// DB 락 - 변경
 // 기본 데이터 입력
 set autocommit true;
 delete from member;
@@ -16,4 +17,24 @@ update member set money = 1000 where member_id = 'memberA';
 commit;
 
 // 세션 2
+commit;
+
+// DB 락 - 조회
+// 기본 데이터 입력
+set autocommit true;
+delete from member;
+insert into member(member_id, money) values ('memberA', 10000);
+
+// 세션 1
+set autocommit false;
+select * from member where member_id = 'memberA' for update;
+
+// 세션 2
+set autocommit false;
+update member set money = 500 where member_id = 'memberA';
+
+// 세션 1 커밋
+commit;
+
+// 세션 2 커밋
 commit;
